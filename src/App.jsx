05c15a3c66858811ab3540e1b637ced5a968d77f
MichelleOliv <michelle482017@gmail.com>
2025-10-28 13:48:14 -0300

    coloquei as paginas em "pages" e arrumei o link delas

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import Navbar from './Components/Navbar/Navbar'
-import Hero from './Components/Hero/Hero'
 import Title from './Components/Title/Title'
-import About from './Components/About/About'
-import Feedbacks from './Components/Feedbacks/Feedbacks'
-import Contact from './Components/Contact/Contact'
 import Footer from './Components/Footer/Footer'
-import Simplifiqueai from './Components/Simplifiqueai/Simplifiqueai'
+import Hero from './Pages/Hero/Hero'
+import About from './Pages/About/About'
+import Feedbacks from './Pages/Feedbacks/Feedbacks'
+import Contact from './Pages/Contact/Contact'
+import Simplifiqueai from './Pages/Simplifiqueai/Simplifiqueai'
 
 const App = () => {
   return (
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -8,9 +8,9 @@ const Navbar = () => {
     <nav className='container'>
         <img src={logo} alt="" className='logo'/>
         <ul>
-            <li><Link to='hero' smooth={true} offset={0} duration={500}>Início</Link></li>
+            <li><Link to='hero-container' smooth={true} offset={0} duration={500}>Início</Link></li>
             <li><Link to='about' smooth={true} offset={-200} duration={500}>Sobre nós</Link></li>
-            <li><Link to='simplifique' smooth={true} offset={-150} duration={500}>Simplifique</Link></li>
+            <li><Link to='simplifiqueai' smooth={true} offset={-200} duration={500}>Simplifique</Link></li>
             <li><Link to='feedbacks' smooth={true} offset={-260} duration={500}>Feedbacks</Link></li>
             <li><Link to='contact' smooth={true} offset={-260} duration={500} className='btn'> Nos contate</Link></li>
         </ul>
