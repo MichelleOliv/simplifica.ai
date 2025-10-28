6fb33bab5f389e2f5e7b1825d0714d733f7377ea
MichelleOliv <michelle482017@gmail.com>
2025-10-28 12:01:12 -0300

    mudei a landing page pra ficar mais acessivel

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,14 +1,23 @@
 import React from 'react'
 import './Hero.css'
-import dark_arrow from '../../assets/dark-arrow.png'
+import estudoIgualitario3 from '../../assets/estudoIgualitario3.jpg'
+import icone from '../../assets/icone.png'
 
 const Hero = () => {
   return (
     <div className='hero-container'>
-      <div className='hero-text'>
-        <h1>Buscamos por um mundo iguaitário</h1>
-        <p>Pois a educação é para todos</p>
-        <button className='btn'>Simplificar</button>
+      <div className='hero-content'>
+        <div className='hero-left'>
+          <div className='hero-text'>
+            <img src={icone} alt="icone" />
+            <h1>Buscamos por um mundo iguaitário</h1>
+            <p>Pois a educação é para todos</p>
+            <button className='dark-btn'>Simplificar</button>
+          </div>
+        </div>
+        <div className='hero-right'>
+          <img src={estudoIgualitario3} alt="estudoIgualitario3"/>
+        </div>
       </div>
     </div>
   )
